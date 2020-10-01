1b02139a1b574c31da532a8c1e03fa4864fe0667
Rostyslav Miniukov <thresh1337@gmail.com>
2020-10-01 21:00:31 +0300

    Обучаем шаблон карточки фильма принимать данные

--- src/view/movie-card.js
+++ src/view/movie-card.js
@@ -1,21 +1,30 @@
+// Получаем название класса для активной кнопки
+const isControlItemActive = (item) => item ? `film-card__controls-item--active` : ``;
+
 // Фунция создания шаблона карточки фильма
-export const createMovieCardTemplate = () => {
+export const createMovieCardTemplate = (movie) => {
+  const {comments} = movie;
+  const {filmInfo: {title, totalRating, poster, runtime, description, release, genres}} = movie;
+  const {userDetails: {watchlist, favorite, alreadyWatched}} = movie;
+
+  const releaseYear = release.date.getFullYear();
+
   return (
     `<article class="film-card">
-      <h3 class="film-card__title">The Dance of Life</h3>
-      <p class="film-card__rating">8.3</p>
+      <h3 class="film-card__title">${title}</h3>
+      <p class="film-card__rating">${totalRating}</p>
       <p class="film-card__info">
-        <span class="film-card__year">1929</span>
-        <span class="film-card__duration">1h 55m</span>
-        <span class="film-card__genre">Musical</span>
+        <span class="film-card__year">${releaseYear}</span>
+        <span class="film-card__duration">${runtime}</span>
+        <span class="film-card__genre">${genres[0]}</span>
       </p>
-      <img src="./images/posters/the-dance-of-life.jpg" alt="" class="film-card__poster">
-      <p class="film-card__description">Burlesque comic Ralph "Skid" Johnson (Skelly), and specialty dancer Bonny Lee King (Carroll), end up together on a cold, rainy night at a tr…</p>
-      <a class="film-card__comments">5 comments</a>
+      <img src="${poster}" alt="${title}" class="film-card__poster">
+      <p class="film-card__description">${description}</p>
+      <a class="film-card__comments">${comments.length} comments</a>
       <form class="film-card__controls">
-        <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
-        <button class="film-card__controls-item button film-card__controls-item--mark-as-watched">Mark as watched</button>
-        <button class="film-card__controls-item button film-card__controls-item--favorite">Mark as favorite</button>
+        <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist ${isControlItemActive(watchlist)}">Add to watchlist</button>
+        <button class="film-card__controls-item button film-card__controls-item--mark-as-watched ${isControlItemActive(alreadyWatched)}">Mark as watched</button>
+        <button class="film-card__controls-item button film-card__controls-item--favorite ${isControlItemActive(favorite)}">Mark as favorite</button>
       </form>
     </article>`
   );
