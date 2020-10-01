6465fa55153ab0a01ec68e7467c9429d44f01c1f
Rostyslav Miniukov <thresh1337@gmail.com>
2020-10-02 02:05:03 +0300

    Используем моки и описываем логику допоказа фильмов, а также рефакторим код

--- src/main.js
+++ src/main.js
@@ -8,10 +8,13 @@ import {createShowMoreButtonTemplate} from './view/show-more-button';
 // import {createMovieDetailsTemplate} from './view/movie-details';
 import {createFooterStatsTemplate} from './view/footer-stats';
 import {generateMovie} from './mock/movie';
+import {generateFilter} from './mock/filter';
+import {generateUser} from './mock/user';
 
 // Количество карточек фильмов в блоках
-const FILMS_AMOUNT = {
-  DEFAULT: 5,
+const MOVIES_AMOUNT = {
+  TOTAL: 20,
+  PER_STEP: 5,
   TOP_RATED: 2,
   MOST_COMMENTED: 2
 };
@@ -22,18 +25,14 @@ const EXTRA_BLOCK_TITLE = {
   MOST_COMMENTED: `Most commented`
 };
 
-const movies = new Array(FILMS_AMOUNT.DEFAULT).fill().map(generateMovie);
+// Генерируем необходимые данные для заполнения
+const movies = new Array(MOVIES_AMOUNT.TOTAL).fill().map(generateMovie);
+const filters = generateFilter(movies);
+const user = generateUser();
 
 // Функция отрисовки компонента на страницу
 const renderComponent = (container, component, place = `beforeend`) => {
   container.insertAdjacentHTML(place, component);
-};
-
-// Функция отрисовки карточек фильмов
-const renderFilmCards = (container, amount) => {
-  for (let i = 0; i < amount; i++) {
-    renderComponent(container, createMovieCardTemplate(movies[i]));
-  }
 };
 
 // Функция получения элемента контейнера по заголовку
@@ -43,25 +42,63 @@ const getExtraListContainerNode = (title) => {
   return extraBlockNode.querySelector(`.films-list__container`);
 };
 
-// Отрисовываем необходимые компоненты
+// Отрисовываем информацию о пользователе
 const siteHeaderNode = document.querySelector(`.header`);
-renderComponent(siteHeaderNode, createUserRankTemplate());
+renderComponent(siteHeaderNode, createUserRankTemplate(user));
 
+// Отрисовываем элементы сортировки и фильтры
 const siteMainNode = document.querySelector(`.main`);
-renderComponent(siteMainNode, createSiteMenuTemplate());
+renderComponent(siteMainNode, createSiteMenuTemplate(filters));
 renderComponent(siteMainNode, createFilmsContainerTemplate());
 
+// Отрисовываем карточки фильмов
 const filmsListNode = siteMainNode.querySelector(`.films-list__container`);
-renderFilmCards(filmsListNode, FILMS_AMOUNT.DEFAULT);
+movies
+  .slice(0, MOVIES_AMOUNT.PER_STEP)
+  .forEach((movie) => renderComponent(filmsListNode, createMovieCardTemplate(movie)));
 
-const filmsContainerNode = siteMainNode.querySelector(`.films-list`);
-renderComponent(filmsContainerNode, createShowMoreButtonTemplate());
+// Отрисовываем кнопку 'Show More' по надобности
+if (movies.length > MOVIES_AMOUNT.PER_STEP) {
+  let renderedMoviesCount = MOVIES_AMOUNT.PER_STEP;
 
+  const filmsContainerNode = siteMainNode.querySelector(`.films-list`);
+  renderComponent(filmsContainerNode, createShowMoreButtonTemplate());
+
+  const showMoreButton = document.querySelector(`.films-list__show-more`);
+  showMoreButton.addEventListener(`click`, (evt) => {
+    evt.preventDefault();
+
+    movies
+      .slice(renderedMoviesCount, renderedMoviesCount + MOVIES_AMOUNT.PER_STEP)
+      .forEach((movie) => renderComponent(filmsListNode, createMovieCardTemplate(movie)));
+
+    renderedMoviesCount += MOVIES_AMOUNT.PER_STEP;
+
+    if (renderedMoviesCount >= movies.length) {
+      showMoreButton.remove();
+    }
+  });
+}
+
+// Отрисовываем дополнительные блоки
 const filmsParentNode = siteMainNode.querySelector(`.films`);
+// Отрисовываем блок с высшем рейтингом фильмов
 renderComponent(filmsParentNode, createExtraFilmContainerTemplate(EXTRA_BLOCK_TITLE.TOP_RATED));
-renderFilmCards(getExtraListContainerNode(EXTRA_BLOCK_TITLE.TOP_RATED), FILMS_AMOUNT.TOP_RATED);
-renderComponent(filmsParentNode, createExtraFilmContainerTemplate(EXTRA_BLOCK_TITLE.MOST_COMMENTED));
-renderFilmCards(getExtraListContainerNode(EXTRA_BLOCK_TITLE.MOST_COMMENTED), FILMS_AMOUNT.MOST_COMMENTED);
+movies
+  .sort((a, b) => b.filmInfo.totalRating - a.filmInfo.totalRating)
+  .slice(0, MOVIES_AMOUNT.TOP_RATED)
+  .forEach((movie) => renderComponent(getExtraListContainerNode(EXTRA_BLOCK_TITLE.TOP_RATED), createMovieCardTemplate(movie)));
 
+// Отрисовываем блок с большим количеством комментариев фильмов
+renderComponent(filmsParentNode, createExtraFilmContainerTemplate(EXTRA_BLOCK_TITLE.MOST_COMMENTED));
+movies
+  .sort((a, b) => b.comments.length - a.comments.length)
+  .slice(0, MOVIES_AMOUNT.MOST_COMMENTED)
+  .forEach((movie) => renderComponent(getExtraListContainerNode(EXTRA_BLOCK_TITLE.MOST_COMMENTED), createMovieCardTemplate(movie)));
+
+// Отрисовываем футер
 const footerStatictsNode = document.querySelector(`.footer__statistics`);
 renderComponent(footerStatictsNode, createFooterStatsTemplate(movies.length));
+
+// Отрисовываем попап с полной информацией о фильме
+// renderComponent(document.body, createMovieDetailsTemplate(movies[0]));
