3d238f75157c5673930f09e6a4278fd007adf215
Rostyslav Miniukov <thresh1337@gmail.com>
2020-10-20 00:01:24 +0300

    Реализуем безопасный вввод

--- src/view/movie-details.js
+++ src/view/movie-details.js
@@ -2,6 +2,7 @@ import SmartView from './smart';
 import {getFormattedReleaseDate, getFormattedRuntime, getHumanizeCommentDate} from '../utils/date';
 import {getRandomElement} from '../utils/common';
 import {AUTHORS} from '../const';
+import he from 'he';
 
 const EmojiType = {
   SMILE: `smile`,
@@ -30,7 +31,7 @@ const createCommentItemTemplate = (commentItem) => {
         <img src="${emojiPath}" width="55" height="55" alt="emoji-${emoji}">
       </span>
       <div>
-        <p class="film-details__comment-text">${comment}</p>
+        <p class="film-details__comment-text">${he.encode(comment)}</p>
         <p class="film-details__comment-info">
           <span class="film-details__comment-author">${author}</span>
           <span class="film-details__comment-day">${formattedDate}</span>
@@ -163,7 +164,7 @@ const createFilmDetailsTemplate = (movie) => {
               <div for="add-emoji" class="film-details__add-emoji-label">${selectedEmojiTemplate}</div>
 
               <label class="film-details__comment-label">
-                <textarea class="film-details__comment-input" placeholder="Select reaction below and write comment here" name="comment">${userText ? userText : ``}</textarea>
+                <textarea class="film-details__comment-input" placeholder="Select reaction below and write comment here" name="comment">${userText ? he.encode(userText) : ``}</textarea>
               </label>
 
               <div class="film-details__emoji-list">
