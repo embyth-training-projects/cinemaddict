a8233c5cba95b15828b7a216c7842cd15353226a
Rostyslav Miniukov <thresh1337@gmail.com>
2020-10-21 18:35:16 +0300

    Добавляем код графиков

--- src/view/statistics.js
+++ src/view/statistics.js
@@ -59,7 +59,66 @@ const createStatisticsTemplate = (data) => {
 };
 
 const renderStatisticsChart = (statisticsCtx, films) => {
+  const BAR_HEIGHT = 50;
+  statisticsCtx.height = BAR_HEIGHT * Object.keys(getGenresFrequencies(films)).length;
 
+  return new Chart(statisticsCtx, {
+    plugins: [ChartDataLabels],
+    type: `horizontalBar`,
+    data: {
+      labels: [`Sci-Fi`, `Animation`, `Fantasy`,
+        `Comedy`, `TV Series`],
+      datasets: [{
+        data: [11, 8, 7, 4, 3],
+        backgroundColor: `#ffe800`,
+        hoverBackgroundColor: `#ffe800`,
+        anchor: `start`,
+        barThickness: 24,
+      }]
+    },
+    options: {
+      plugins: {
+        datalabels: {
+          font: {
+            size: 20
+          },
+          color: `#ffffff`,
+          anchor: `start`,
+          align: `start`,
+          offset: 40,
+        }
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            fontColor: `#ffffff`,
+            padding: 100,
+            fontSize: 20
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false
+          },
+        }],
+        xAxes: [{
+          ticks: {
+            display: false,
+            beginAtZero: true
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false
+          },
+        }],
+      },
+      legend: {
+        display: false
+      },
+      tooltips: {
+        enabled: false
+      },
+    }
+  });
 };
 
 export default class Statistics extends SmartView {
