b8e9b49926b400c0e3327089158a1e05589aeba3
Rostyslav Miniukov <thresh1337@gmail.com>
2020-10-25 18:55:33 +0200

    Реализуем паттерн Адаптер

--- src/model/films.js
+++ src/model/films.js
@@ -62,4 +62,82 @@ export default class Films extends Observer {
 
     this._notify(updateType, update);
   }
+
+  static adaptToClient(film) {
+    const adaptedFilm = Object.assign(
+        {},
+        film,
+        {
+          actors: film.film_info.actors,
+          ageRating: film.film_info.age_rating,
+          alternativeTitle: film.film_info.alternative_title,
+          description: film.film_info.description,
+          director: film.film_info.director,
+          genres: film.film_info.genre,
+          poster: film.film_info.poster,
+          release: film.film_info.release,
+          runtime: film.film_info.runtime,
+          title: film.film_info.title,
+          totalRating: film.film_info.total_rating,
+          writers: film.film_info.writers,
+          isWatchlisted: film.user_details.watchlist,
+          isFavorite: film.user_details.favorite,
+          isWatched: film.user_details.already_watched,
+          watchingDate: film.user_details.watching_date
+        }
+    );
+
+    delete adaptedFilm.user_details;
+    delete adaptedFilm.film_info;
+
+    return adaptedFilm;
+  }
+
+  static adaptToServer(film) {
+    const adaptedFilm = Object.assign(
+        {},
+        film,
+        {
+          "film_info": {
+            "actors": film.actors,
+            "age_rating": film.ageRating,
+            "alternative_title": film.alternativeTitle,
+            "description": film.description,
+            "director": film.director,
+            "genre": film.genres,
+            "poster": film.poster,
+            "release": film.release,
+            "runtime": film.runtime,
+            "title": film.title,
+            "total_rating": film.totalRating,
+            "writers": film.writers
+          },
+          "user_details": {
+            "watchlist": film.isWatchlisted,
+            "favorite": film.isFavorite,
+            "already_watched": film.isWatched,
+            "watching_date": film.watchingDate
+          }
+        }
+    );
+
+    delete adaptedFilm.actors;
+    delete adaptedFilm.ageRating;
+    delete adaptedFilm.alternativeTitle;
+    delete adaptedFilm.description;
+    delete adaptedFilm.director;
+    delete adaptedFilm.genres;
+    delete adaptedFilm.poster;
+    delete adaptedFilm.release;
+    delete adaptedFilm.runtime;
+    delete adaptedFilm.title;
+    delete adaptedFilm.totalRating;
+    delete adaptedFilm.writers;
+    delete adaptedFilm.isWatchlisted;
+    delete adaptedFilm.isFavorite;
+    delete adaptedFilm.isWatched;
+    delete adaptedFilm.watchingDate;
+
+    return adaptedFilm;
+  }
 }
