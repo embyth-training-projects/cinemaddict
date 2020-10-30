7a66e33c03e65c39c0650ec9c9046f09552e152e
Rostyslav Miniukov <thresh1337@gmail.com>
2020-10-30 20:01:00 +0200

    Рефакторинг хранилища

--- src/api/store.js
+++ src/api/store.js
@@ -19,27 +19,64 @@ export default class Store {
     );
   }
 
-  setItem(key, value) {
+  setFilm(key, value) {
     const store = this.getItems();
+
+    const updatedStore = {
+      films: Object.assign(store.films, {
+        [key]: value
+      }),
+      comments: store.comments
+    };
 
     this._storage.setItem(
         this._storeKey,
         JSON.stringify(
-            Object.assign({}, store, {
-              [key]: value
-            })
+            Object.assign({}, updatedStore)
         )
     );
   }
 
-  removeItem(key) {
+  setComments(key, value) {
     const store = this.getItems();
 
-    delete store[key];
+    const updatedStore = {
+      films: store.films,
+      comments: Object.assign(store.comments, {
+        [key]: value
+      })
+    };
 
     this._storage.setItem(
         this._storeKey,
-        JSON.stringify(store)
+        JSON.stringify(
+            Object.assign({}, updatedStore)
+        )
+    );
+  }
+
+  removeComment(key) {
+    const store = this.getItems();
+
+    for (let filmKey in store.films) {
+      if (store.films[filmKey].comments.find((id) => id === key)) {
+        const index = store.films[filmKey].comments.indexOf(key);
+        const updatedComments = [...store.comments[filmKey].slice(0, index), ...store.comments[filmKey].slice(index + 1)];
+        const updatedCommentsIds = [...store.films[filmKey].comments.slice(0, index), ...store.films[filmKey].comments.slice(index + 1)];
+        store.comments[filmKey] = updatedComments;
+        store.films[filmKey].comments = updatedCommentsIds;
+        break;
+      }
+    }
+
+    const updatedStore = {
+      films: store.films,
+      comments: store.comments
+    };
+
+    this._storage.setItem(
+        this._storeKey,
+        JSON.stringify(Object.assign({}, updatedStore))
     );
   }
 }
